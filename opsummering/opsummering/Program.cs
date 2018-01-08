3e7541ab3476c3acf574dca9e3c074cdf0a8d1f4
KasperMikkelsen <mikkelsenkasper75@gmail.com>
2018-01-08 10:15:11 +0100

    added code

---
+++ opsummering/opsummering/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opsummering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
