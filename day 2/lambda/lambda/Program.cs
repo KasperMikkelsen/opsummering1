fe159dde19c3623707a91a1dc5e261bd4dac36fb
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2018-01-09 14:44:21 +0100

    Lamda3

--- day 2/lambda/lambda/Program.cs
+++ day 2/lambda/lambda/Program.cs
@@ -8,13 +8,25 @@ namespace lambda
 {
     class Program
     {
-        delegate int NumberDelegat(int input); 
+        delegate void NumberDelegat();
+
+		static void Message() {
+			Console.WriteLine("Hello " + " ");
+		}
         static void Main(string[] args)
         {
-            NumberDelegat timesTwo = x => x * 2;
-            Console.WriteLine(timesTwo(7));
-            Console.ReadLine();
-            
+			//NumberDelegat timesTwo = x => x * 2;
+			//Console.WriteLine(timesTwo(40));
+			//Console.ReadLine
+			NumberDelegat del=Message;
+			del();
+
+			NumberDelegat del1 = () => Console.WriteLine("World");
+			del1();
+
+			NumberDelegat del2 = delegate () { Console.WriteLine("ddd"); };
+			del2();
+			Console.ReadKey();
         }
     }
 }
