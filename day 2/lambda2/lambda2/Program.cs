0eea8cf744522136599542a6266d633514d54a92
KasperMikkelsen <mikkelsenkasper75@gmail.com>
2018-01-09 10:46:53 +0100

    added some code to lambda2

--- day 2/lambda2/lambda2/Program.cs
+++ day 2/lambda2/lambda2/Program.cs
@@ -8,8 +8,12 @@ namespace lambda2
 {
     class Program
     {
+        delegate float NumberDelegate(float x , float y, float z);
         static void Main(string[] args)
         {
+            NumberDelegate multiply = (x, y, z) => x + y + z;
+            Console.WriteLine(multiply(2, 3, 4));
+            Console.ReadLine();
         }
     }
 }
