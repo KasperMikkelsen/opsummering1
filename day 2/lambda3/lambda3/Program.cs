4485edad08ec5d7fe93cafd5011a564f7039c7ec
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2018-01-09 14:46:46 +0100

    Change code lamda3

--- day 2/lambda3/lambda3/Program.cs
+++ day 2/lambda3/lambda3/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lambda3
+namespace lambda
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		delegate void NumberDelegat();
+
+		static void Message()
+		{
+			Console.WriteLine("Hello " + " ");
+		}
+		static void Main(string[] args)
+		{
+			//NumberDelegat timesTwo = x => x * 2;
+			//Console.WriteLine(timesTwo(40));
+			//Console.ReadLine
+			NumberDelegat del = Message;
+			del();
+
+			NumberDelegat del1 = () => Console.WriteLine("World");
+			del1();
+
+			NumberDelegat del2 = delegate () { Console.WriteLine("ddd"); };
+			del2();
+			Console.ReadKey();
+		}
+	}
 }
