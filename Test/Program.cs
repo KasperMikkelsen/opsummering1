798cb28955b9ac6c1d5646a0d3899e167bc70826
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2018-01-10 10:30:44 +0100

    Test

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MultithreadingApplication
+{
+
+	class ThreadCreationProgram
+	{
+
+		public static void CallToChildThread()
+		{
+			Console.WriteLine("Help");
+		}
+
+		static void Main(string[] args)
+		{
+			ThreadStart childref = new ThreadStart(CallToChildThread);
+			Console.WriteLine("Me Please");
+			Thread childThread = new Thread(childref);
+			childThread.Start();
+			Console.ReadKey();
+		}
+	}
+}
