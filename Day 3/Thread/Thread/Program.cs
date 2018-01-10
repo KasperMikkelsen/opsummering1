36fba87a831f573c1e70643e8131c428eb012783
KasperMikkelsen <mikkelsenkasper75@gmail.com>
2018-01-10 10:32:48 +0100

    trying to make a Thread might be working

--- Day 3/Thread/Thread/Program.cs
+++ Day 3/Thread/Thread/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace Thread
+namespace Program
+
 {
-    class Program
+
+    class MakeingAThread
     {
+
+        
+
         static void Main(string[] args)
         {
+            ThreadStart t1ref = new ThreadStart(Thread1);
+            Thread thread1 = new Thread(t1ref);
+            thread1.Start();
+            ThreadStart t2ref = new ThreadStart(Thread2);
+            Thread thread2 = new Thread(t2ref);
+            thread2.Start();
+            Console.ReadKey();
+        }
+        public static void Thread1()
+        {
+            Console.WriteLine("t1 starts");
+        }
+
+        public static void Thread2()
+        {
+            Console.WriteLine("t2 starts");
         }
     }
+
 }
